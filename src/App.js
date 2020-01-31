35164b825d961c6ea35e3a4eb70c00eb6e7ee46a
alexis villaraza <alexis.borja@gmail.com>
2020-01-31 09:36:41 -0700

    coded CharacterList.js imported and routed in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import {Route} from 'react-router-dom';
 import Header from "./components/Header.js";
+import Welcome from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
+import CharacterCard from './components/CharacterCard';
+
+ 
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Welcome />
+      <Route exact path ='/' component={CharacterList} />
+      
     </main>
   );
 }
